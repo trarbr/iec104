9abf6d963adf633a12e2491699db3c994eca1070
Troels Brødsgaard <troels@uptime.dk>
2020-08-15 14:05:41 +0200

    Add ability to receive telegrams and perform sequence synchronization

--- lib/iec104/controlling_connection.ex
+++ lib/iec104/controlling_connection.ex
@@ -2,36 +2,91 @@ defmodule IEC104.ControllingConnection do
   require Logger
 
   alias IEC104.Frame
-  alias IEC104.Frame.ControlFunction
+  alias IEC104.Frame.{ControlFunction, InformationTransfer, SupervisoryFunction}
 
   @behaviour :gen_statem
 
-  defstruct [:host, :port, :handler, :connect_timeout, :connect_backoff, :socket]
+  defstruct [
+    :host,
+    :port,
+    :handler,
+    :connect_timeout,
+    :connect_backoff,
+    :response_timeout,
+    :sequence_synchronization_timeout,
+    :sequence_synchronization_threshold,
+    :data_transfer_idle_timeout,
+    :buffer,
+    :socket,
+    :received_sequence_number,
+    :acknowledged_received_sequence_number,
+    :sequence_synchronization_timer_running?
+  ]
 
   @options_schema [
     host: [
       type: :any,
-      doc: "inet:socket_address() | inet:hostname()",
+      doc: "The IP address or hostname to connect to.",
       default: 'localhost'
     ],
     port: [
       type: :pos_integer,
-      doc: "a port number",
+      doc: "The port number to connect to.",
       default: 2404
     ],
     handler: [
       type: :pid,
-      doc: "default: self()"
+      doc: """
+      The PID of the handler process for the connection. The connection
+      process will notify the handler process on connection events, such as
+      disconnects. Default: self().
+      """
     ],
     connect_timeout: [
       type: :pos_integer,
-      doc: "How long to wait before timing out a connection attempt",
+      doc: "How long to wait before timing out a connection attempt.",
       default: 5000
     ],
     connect_backoff: [
       type: :pos_integer,
-      doc: "How long to wait before retrying connect",
+      doc: "How long to wait before attempting to re-establish a connection.",
       default: 5000
+    ],
+    response_timeout: [
+      type: :pos_integer,
+      doc: """
+      The maximum time to wait (in milliseconds) for a response from the
+      controlled station before actively closing the connection. This timeout
+      is called `t1` by the standard. Minimum is 1000 ms, maximum is 255_000
+      ms.
+      """,
+      default: 15_000
+    ],
+    sequence_synchronization_timeout: [
+      type: :pos_integer,
+      doc: """
+      The maximum time to wait (in milliseconds) before sending a receipt for
+      received telegrams. This timeout is called t2 by the standard. Minimum
+      is 1000 ms, maximum is 255_000 ms.
+      """,
+      default: 10_000
+    ],
+    sequence_synchronization_threshold: [
+      type: :pos_integer,
+      doc: """
+      The maximum number of telegrams that can be received before sending a
+      receipt.
+      """,
+      default: 8
+    ],
+    data_transfer_idle_timeout: [
+      type: :pos_integer,
+      doc: """
+      The maximum time (in milliseconds) that the connection may be idle
+      before sending a test frame. This timeout is called t3 by the standard.
+      Minimum is 1000 ms, maximum is 172_800_000 ms (48h).
+      """,
+      default: 20_000
     ]
   ]
 
@@ -59,7 +114,16 @@ defmodule IEC104.ControllingConnection do
       port: Keyword.fetch!(args, :port),
       handler: Keyword.fetch!(args, :handler),
       connect_timeout: Keyword.fetch!(args, :connect_timeout),
-      connect_backoff: Keyword.fetch!(args, :connect_backoff)
+      connect_backoff: Keyword.fetch!(args, :connect_backoff),
+      response_timeout: Keyword.fetch!(args, :response_timeout),
+      sequence_synchronization_timeout: Keyword.fetch!(args, :sequence_synchronization_timeout),
+      sequence_synchronization_threshold:
+        Keyword.fetch!(args, :sequence_synchronization_threshold),
+      data_transfer_idle_timeout: Keyword.fetch!(args, :data_transfer_idle_timeout),
+      buffer: <<>>,
+      received_sequence_number: 0,
+      acknowledged_received_sequence_number: 0,
+      sequence_synchronization_timer_running?: false
     }
 
     actions = [{:next_event, :internal, :connect}]
@@ -77,26 +141,36 @@ defmodule IEC104.ControllingConnection do
 
       error ->
         Logger.info("could not connect: #{inspect(error)}")
-
-        {:keep_state_and_data, [{:state_timeout, data.connect_backoff, :connect}]}
+        {:keep_state_and_data, [{:next_event, :internal, :connect}]}
     end
   end
 
   @doc false
   def connected(:info, {:tcp_closed, _port}, data) do
-    {:next_state, :disconnected, %{data | socket: nil}}
+    {:next_state, :disconnected, %{data | socket: nil},
+     [{:state_timeout, data.connect_backoff, :connect}]}
   end
 
   def connected(:info, {:tcp, _port, frame}, data) do
-    case Frame.decode(frame) do
-      {:ok, %ControlFunction{function: :start_data_transfer_confirmation}, _rest} ->
-        # TODO: Transfer to new state?
-        :keep_state_and_data
+    {:keep_state, %{data | buffer: data.buffer <> frame},
+     [{:next_event, :internal, :handle_frame}]}
+  end
 
-      {:ok, %ControlFunction{function: :test_frame_activation}, _rest} ->
-        %ControlFunction{function: :test_frame_confirmation}
-        |> send_frame(data)
+  def connected(:internal, :handle_frame, data) do
+    data.buffer
+    |> Frame.decode()
+    |> case do
+      {:ok, %ControlFunction{function: :start_data_transfer_confirmation}, rest} ->
+        _ = notify_handler(data, :data_transfer)
 
+        {data, actions} =
+          {%{data | buffer: rest}, []}
+          |> data_transfer_idle_timeout()
+          |> handle_frame()
+
+        {:next_state, :data_transfer, data, actions}
+
+      {:error, :in_frame} ->
         :keep_state_and_data
     end
   end
@@ -105,16 +179,144 @@ defmodule IEC104.ControllingConnection do
     %ControlFunction{function: :start_data_transfer_activation}
     |> send_frame(data)
 
-    {:keep_state_and_data, [{:reply, from, :ok}]}
+    {:keep_state_and_data,
+     [{:reply, from, :ok}, {:state_timeout, data.response_timeout, :response}]}
+  end
+
+  def connected(:state_timeout, :response, data) do
+    :ok = :gen_tcp.close(data.socket)
+
+    {:next_state, :disconnected, %{data | socket: nil},
+     [{:state_timeout, data.connect_backoff, :connect}]}
+  end
+
+  def data_transfer(:info, {:tcp_closed, _port}, data) do
+    {:next_state, :disconnected, %{data | socket: nil},
+     [{:state_timeout, data.connect_backoff, :connect}]}
+  end
+
+  def data_transfer(:info, {:tcp, _port, frame}, data) do
+    {:keep_state, %{data | buffer: data.buffer <> frame},
+     [{:next_event, :internal, :handle_frame}]}
+  end
+
+  def data_transfer(:internal, :handle_frame, data) do
+    data.buffer
+    |> Frame.decode()
+    |> case do
+      {:ok, %ControlFunction{function: :test_frame_activation}, rest} ->
+        %ControlFunction{function: :test_frame_confirmation}
+        |> send_frame(data)
+
+        {data, actions} =
+          {%{data | buffer: rest}, []}
+          |> data_transfer_idle_timeout()
+          |> handle_frame()
+
+        {:keep_state, data, actions}
+
+      {:ok, %ControlFunction{function: :test_frame_confirmation}, rest} ->
+        {data, actions} =
+          {%{data | buffer: rest}, [{:state_timeout, :cancel}]}
+          |> data_transfer_idle_timeout()
+          |> handle_frame()
+
+        {:keep_state, data, actions}
+
+      {:ok, %InformationTransfer{} = frame, rest} ->
+        _ = notify_handler(data, frame.telegram)
+
+        # TODO: Might have to cancel the state_timeout here, if the sequence counter matches expected
+        {data, actions} =
+          {%{data | buffer: rest, received_sequence_number: frame.sent_sequence_number}, []}
+          |> data_transfer_idle_timeout()
+          |> sequence_synchronization()
+          |> handle_frame()
+
+        {:keep_state, data, actions}
+
+      {:error, :in_frame} ->
+        :keep_state_and_data
+    end
+  end
+
+  def data_transfer(:internal, :sequence_synchronization, data) do
+    send_supervisory_function(data)
+  end
+
+  def data_transfer({:timeout, :sequence_synchronization}, :data_transfer, data) do
+    send_supervisory_function(data)
+  end
+
+  def data_transfer({:timeout, :idle}, :data_transfer, data) do
+    %ControlFunction{function: :test_frame_activation}
+    |> send_frame(data)
+
+    {:keep_state_and_data, [{:state_timeout, data.response_timeout, :response}]}
+  end
+
+  def data_transfer(:state_timeout, :response, data) do
+    :ok = :gen_tcp.close(data.socket)
+
+    {:next_state, :disconnected, %{data | socket: nil},
+     [{:state_timeout, data.connect_backoff, :connect}]}
+  end
+
+  defp send_supervisory_function(data) do
+    %SupervisoryFunction{
+      received_sequence_number: data.received_sequence_number + 1
+    }
+    |> send_frame(data)
+
+    {:keep_state,
+     %{
+       data
+       | sequence_synchronization_timer_running?: false,
+         acknowledged_received_sequence_number: data.received_sequence_number + 1
+     }}
   end
 
   defp send_frame(frame, data) do
     {:ok, frame} = Frame.encode(frame)
-
     :ok = :gen_tcp.send(data.socket, frame)
   end
 
   defp notify_handler(data, state) do
     send(data.handler, {__MODULE__, state})
+  end
+
+  defp data_transfer_idle_timeout({data, actions}) do
+    {data, actions ++ [{{:timeout, :idle}, data.data_transfer_idle_timeout, :data_transfer}]}
+  end
+
+  defp sequence_synchronization({data, actions}) do
+    cond do
+      sequence_synchronization_threshold_reached?(data) ->
+        {data, actions ++ [{:next_event, :internal, :sequence_synchronization}]}
+
+      not data.sequence_synchronization_timer_running? ->
+        {%{data | sequence_synchronization_timer_running?: true},
+         actions ++
+           [
+             {{:timeout, :sequence_synchronization}, data.sequence_synchronization_timeout,
+              :data_transfer}
+           ]}
+
+      true ->
+        {data, actions}
+    end
+  end
+
+  defp sequence_synchronization_threshold_reached?(data) do
+    abs(data.acknowledged_received_sequence_number - data.received_sequence_number) >=
+      data.sequence_synchronization_threshold
+  end
+
+  defp handle_frame({data, actions}) do
+    if data.buffer == <<>> do
+      {data, actions}
+    else
+      {data, actions ++ [{:next_event, :internal, :handle_frame}]}
+    end
   end
 end
--- test/iec104/controlling_connection_test.exs
+++ test/iec104/controlling_connection_test.exs
@@ -3,77 +3,240 @@ defmodule IEC104.ControllingConnectionTest do
 
   import ExUnit.CaptureLog
 
-  alias IEC104.{ControllingConnection, Frame}
-  alias IEC104.Frame.ControlFunction
+  alias IEC104.{ControllingConnection, Frame, Telegram, InformationObject}
+  alias IEC104.Frame.{ControlFunction, InformationTransfer, SupervisoryFunction}
+  alias IEC104.InformationElement.QDS
 
   describe "connecting" do
     setup :server_socket
 
     test "attempts to connect when started", context do
-      {:ok, _pid} = ControllingConnection.start_link(port: context.port)
+      {:ok, _pid} = ControllingConnection.start_link(port: context.listen_port)
 
-      assert {:ok, socket} = :gen_tcp.accept(context.socket)
+      assert {:ok, socket} = :gen_tcp.accept(context.listen_socket)
       assert_receive {ControllingConnection, :connected}
     end
 
     test "keeps trying if it fails to connect", context do
-      :ok = :gen_tcp.close(context.socket)
+      :ok = :gen_tcp.close(context.listen_socket)
 
       start_and_wait = fn ->
-        ControllingConnection.start_link(port: context.port, connect_backoff: 10)
-        Process.sleep(100)
+        ControllingConnection.start_link(port: context.listen_port, connect_backoff: 10)
+        Process.sleep(250)
       end
 
       assert capture_log(start_and_wait) =~ "could not connect"
 
-      {:ok, socket} = :gen_tcp.listen(context.port, [:binary])
+      {:ok, listen_socket} = :gen_tcp.listen(context.listen_port, [:binary])
 
-      assert {:ok, socket} = :gen_tcp.accept(socket)
+      assert {:ok, socket} = :gen_tcp.accept(listen_socket)
       assert_receive {ControllingConnection, :connected}
     end
   end
 
   describe "connected" do
-    setup :server_socket
+    setup [:server_socket]
+
+    test "transitions to data transfer on confirmation", context do
+      context = connect(context)
+      ControllingConnection.start_data_transfer(context.conn)
+
+      assert {:ok, frame} = :gen_tcp.recv(context.socket, 0)
+
+      assert {:ok, %ControlFunction{function: :start_data_transfer_activation}, ""} ==
+               Frame.decode(frame)
+
+      %ControlFunction{function: :start_data_transfer_confirmation}
+      |> send_frame(context.socket)
+
+      assert_receive {ControllingConnection, :data_transfer}
+    end
+
+    test "disconnects if test frame confirmation is not received", context do
+      context = connect(context, response_timeout: 1)
+      ControllingConnection.start_data_transfer(context.conn)
+
+      {:ok, _start_data_transfer_activation_frame} = :gen_tcp.recv(context.socket, 0)
+
+      assert {:error, :closed} = :gen_tcp.recv(context.socket, 0)
+    end
+  end
+
+  describe "data_transfer" do
+    setup [:server_socket]
 
     test "responds to test frames", context do
-      {:ok, _pid} = ControllingConnection.start_link(port: context.port)
-      {:ok, socket} = :gen_tcp.accept(context.socket)
+      context = context |> connect() |> data_transfer()
 
       %ControlFunction{function: :test_frame_activation}
-      |> send_frame(socket)
+      |> send_frame(context.socket)
 
-      assert {:ok, frame} = :gen_tcp.recv(socket, 0)
+      assert {:ok, frame} = :gen_tcp.recv(context.socket, 0)
 
       assert {:ok, %ControlFunction{function: :test_frame_confirmation}, ""} ==
                Frame.decode(frame)
     end
 
-    @tag :not_implemented
-    test "sends test frames when nothing happens" do
-      assert false
+    test "sends test frames when nothing happens", context do
+      context =
+        context
+        |> connect(data_transfer_idle_timeout: 1)
+        |> data_transfer()
+
+      assert {:ok, frame} = :gen_tcp.recv(context.socket, 0)
+
+      assert {:ok, %ControlFunction{function: :test_frame_activation}, ""} ==
+               Frame.decode(frame)
     end
 
-    @tag :not_implemented
-    test "sends supervisory frames when it receives a telegram, with the correct sequence numbers" do
-      assert false
+    test "notifies handler when it receives a telegram", context do
+      context = context |> connect() |> data_transfer()
+
+      %InformationTransfer{
+        received_sequence_number: 0,
+        sent_sequence_number: 0,
+        telegram: telegram()
+      }
+      |> send_frame(context.socket)
+
+      assert_receive {ControllingConnection, %IEC104.Telegram{} = telegram}
+      assert telegram() == telegram
     end
 
-    @tag :not_implemented
-    test "can send a telegram" do
-      assert false
+    test "sends supervisory frame on sequence synchronization timeout", context do
+      context =
+        context
+        |> connect(sequence_synchronization_timeout: 1)
+        |> data_transfer()
+
+      Enum.each(0..:rand.uniform(10), fn n ->
+        %InformationTransfer{
+          received_sequence_number: 0,
+          sent_sequence_number: n,
+          telegram: telegram()
+        }
+        |> send_frame(context.socket)
+
+        assert {:ok, frame} = :gen_tcp.recv(context.socket, 0)
+
+        assert {:ok, %SupervisoryFunction{received_sequence_number: n + 1}, ""} ==
+                 Frame.decode(frame)
+      end)
+    end
+
+    test "performs sequence synchronization correctly for a batch of telegrams", context do
+      context =
+        context
+        |> connect(sequence_synchronization_timeout: 100)
+        |> data_transfer()
+
+      last_sent_sequence_number =
+        Enum.reduce(0..2, 1, fn n, _acc ->
+          %InformationTransfer{
+            received_sequence_number: 0,
+            sent_sequence_number: n,
+            telegram: telegram()
+          }
+          |> send_frame(context.socket)
+
+          n
+        end)
+
+      assert {:ok, frame} = :gen_tcp.recv(context.socket, 0)
+
+      assert {:ok, %SupervisoryFunction{received_sequence_number: last_sent_sequence_number + 1},
+              ""} ==
+               Frame.decode(frame)
+    end
+
+    test "performs sequence synchronization when sequence synchronization threshold is reached",
+         context do
+      threshold = 8
+
+      context =
+        context
+        |> connect(sequence_synchronization_threshold: threshold)
+        |> data_transfer()
+
+      Enum.each(0..(threshold - 1), fn n ->
+        %InformationTransfer{
+          received_sequence_number: 0,
+          sent_sequence_number: n,
+          telegram: telegram()
+        }
+        |> send_frame(context.socket)
+      end)
+
+      assert {:error, :timeout} = :gen_tcp.recv(context.socket, 0, 100)
+
+      %InformationTransfer{
+        received_sequence_number: 0,
+        sent_sequence_number: threshold,
+        telegram: telegram()
+      }
+      |> send_frame(context.socket)
+
+      assert {:ok, frame} = :gen_tcp.recv(context.socket, 0, 100)
+
+      assert {:ok, %SupervisoryFunction{received_sequence_number: threshold + 1}, ""} ==
+               Frame.decode(frame)
+    end
+
+    test "disconnects if test frame confirmation is not received", context do
+      context =
+        context
+        |> connect(response_timeout: 1, data_transfer_idle_timeout: 50)
+        |> data_transfer()
+
+      {:ok, _test_frame_activation} = :gen_tcp.recv(context.socket, 0)
+
+      assert {:error, :closed} = :gen_tcp.recv(context.socket, 0)
     end
   end
 
   defp server_socket(context) do
-    {:ok, socket} = :gen_tcp.listen(0, [:binary, {:active, false}])
-    {:ok, port} = :inet.port(socket)
+    {:ok, listen_socket} = :gen_tcp.listen(0, [:binary, {:active, false}])
+    {:ok, listen_port} = :inet.port(listen_socket)
 
-    Map.merge(context, %{socket: socket, port: port})
+    Map.merge(context, %{listen_socket: listen_socket, listen_port: listen_port})
+  end
+
+  defp connect(context, connection_args \\ []) do
+    {:ok, conn} = ControllingConnection.start_link(connection_args ++ [port: context.listen_port])
+    {:ok, socket} = :gen_tcp.accept(context.listen_socket)
+
+    receive do
+      {ControllingConnection, :connected} ->
+        Map.merge(context, %{conn: conn, socket: socket})
+    end
+  end
+
+  defp data_transfer(context) do
+    ControllingConnection.start_data_transfer(context.conn)
+    {:ok, frame} = :gen_tcp.recv(context.socket, 0)
+
+    {:ok, %ControlFunction{function: :start_data_transfer_activation}, ""} = Frame.decode(frame)
+
+    %ControlFunction{function: :start_data_transfer_confirmation}
+    |> send_frame(context.socket)
+
+    context
   end
 
   defp send_frame(frame, socket) do
     {:ok, frame} = Frame.encode(frame)
     :ok = :gen_tcp.send(socket, frame)
+  end
+
+  defp telegram() do
+    %Telegram{
+      type: InformationObject.M_ME_NB_1,
+      cause_of_transmission: :spontaneous,
+      negative_confirmation?: false,
+      test?: false,
+      originator_address: 0,
+      common_address: 12,
+      information_objects: %{12304 => {2494, QDS.new()}}
+    }
   end
 end
